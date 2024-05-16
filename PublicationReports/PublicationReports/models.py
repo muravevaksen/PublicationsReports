ceb772e2859ab54d18205282c712da84d92fd1a9
murav <muravevaksen@gmail.com>
2024-05-16 18:43:39 +0400

    v0.1

--- PublicationReports/PublicationReports/models.py
+++ PublicationReports/PublicationReports/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Publication(models.Model):
-    "Модель публикации"
     id = models.IntegerField(max_length=5)
     author = models.CharField(max_length=50)
     title = models.CharField(max_length=1000)
+    year = models.IntegerField(max_length=4)
+    number = models.CharField(max_length=50)
+    volume = models.CharField(max_length=50)
+    citation = models.CharField(max_length=50)
+    journal = models.CharField(max_length=100)
+    pages = models.CharField(max_length=10)
+    publisher = models.CharField(max_length=100)
