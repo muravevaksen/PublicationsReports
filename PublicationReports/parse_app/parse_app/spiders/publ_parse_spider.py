8fc345417fb42a5bace8fdd814138e9d7b1f6e3d
murav <muravevaksen@gmail.com>
2024-08-16 22:53:38 +0400

    v0.1

--- PublicationReports/parse_app/parse_app/spiders/publ_parse_spider.py
+++ PublicationReports/parse_app/parse_app/spiders/publ_parse_spider.py
@@ -9,7 +9,7 @@ class PublParseSpiderSpider(CrawlSpider):
     name = "publ_parse_spider"
     allowed_domains = ["scholar.google.com"]
     id_author = "9c_OePYAAAAJ"
-    start_urls = ["https://scholar.google.com/citations?view_op=list_works&hl=ru&hl=ru&user="+id_author+"&pagesize=100"]
+    start_urls = ["https://scholar.google.com/citations?view_op=list_works&hl=ru&hl=ru&user=9c_OePYAAAAJ&pagesize=100"]
 
     rules = (Rule(LinkExtractor(restrict_xpaths="//td[@class='gsc_a_t']/a"), callback="parse_item", follow=True),)
 
@@ -18,12 +18,12 @@ class PublParseSpiderSpider(CrawlSpider):
         item = {}
 
         item["title"] = response.xpath('//a[@class="gsc_oci_title_link"]/text()').get()
-        publication = response.xpath('//div[@id="gsc_vcpb"]//div[@class="gsc_oci_value"]')
+        publications = response.xpath('//div[@id="gsc_vcpb"]//div[@class="gsc_oci_value"]')
 
         titles = ['Authors', 'Year', 'journal', 'Volume', 'Number', 'Pages', 'Publisher', '', '', '']
         i = 0
-        for publication in publication:
-            item[titles[i]] = publication.xpath('./text()').get()
+        for p in publications:
+            item[titles[i]] = p.xpath('./text()').get()
             i += 1
 
         return item
