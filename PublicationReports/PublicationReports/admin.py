5e271412f7c25155129b5772109a347f0e5444fd
murav <muravevaksen@gmail.com>
2024-08-21 23:36:11 +0400

    v0.1

--- PublicationReports/PublicationReports/admin.py
+++ PublicationReports/PublicationReports/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Publication
 
 class PiblicationsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'year', 'number', 'volume', 'citation', 'pages', 'id_journal')
+    list_display = ('ID', 'Title', 'Year', 'Number', 'Volume', 'Citation', 'Pages', 'ID_Journal')
 
 admin.site.register(Publication)
--- PublicationReports/PublicationReports/views.py
+++ PublicationReports/PublicationReports/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
-from .models import Publication as PublicationModel
+from .models import Publication as Publ
 from .forms import PublicationForm
 from django.http import JsonResponse
 from django.template.response import TemplateResponse
@@ -8,13 +8,12 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.http import HttpResponseNotFound
 from django.db import models
-def post_list(request):
-    posts = PublicationModel.published.all()
-    return render(request, 'PublicationReports/index.html', {'posts': posts})
+from django.views.decorators.http import require_GET, require_http_methods
 
 def index(request):
+    tittle =
     return TemplateResponse(request,
                             'PublicationReports/index.html',
-                            context={"rubrics": 'текст'
+                            context={"tittle": tittle
                                      }
                             )
