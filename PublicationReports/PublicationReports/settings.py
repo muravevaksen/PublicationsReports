cf1c30e7da89d009140923da7e6a866705ea8d1d
murav <muravevaksen@gmail.com>
2024-08-27 22:44:12 +0400

    v0.1

--- PublicationReports/PublicationReports/settings.py
+++ PublicationReports/PublicationReports/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PublicationReports'
+    'PublicationReports',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
